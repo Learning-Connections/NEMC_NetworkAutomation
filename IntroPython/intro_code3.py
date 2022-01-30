696ac35a8591ed2c68ffbfeb0ff20e3d28df1661
Learning-Connections <info@learningconnections.it>
2022-01-30 05:26:44 -0800

    added intro_code3.py

---
+++ IntroPython/intro_code3.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# """Module docstring."""
+
+"""intro_code3.py
+Data Types, Loops: iterative and conditional, Python Structure,  
+"""
+#Imports
+"""nothing to import"""
+
+#Constants
+"""no constants"""
+
+#Global Variables
+
+#Functions
+
+
+def main():
+    print("Hello!")
+
+#check if this is the main script and execute it
+if __name__ == '__main__':
+        main()
