3ff1c39b166d45168b9a49d56e120a7a6787ba29
Learning-Connections <info@learningconnections.it>
2022-02-01 00:58:30 -0800

    fixed parsingJSON.py

--- IntroPython/parsingJSON.py
+++ IntroPython/parsingJSON.py
@@ -2,10 +2,11 @@
 # """Module docstring."""
 
 """parsingJSON.py
-imports, JSON, data structure, files
+imports, parsin JSON into data structure and files example
 """
 #Imports
 import json
+from pprint import pprint
 
 #Constants
 """no constants"""
@@ -16,3 +17,22 @@ my_file="interface-config.json"
 
 
 #Functions
+def main():
+    # Read in the JSON text
+    with open(my_file, 'r') as f:
+        json_text = f.read()
+        # Display the type and contents of the json_text variable
+        print("json_text is a", type(json_text))
+        print(json_text)
+
+
+        # Use the json module to parse the JSON string into native Python data
+        json_data = json.loads(json_text)
+
+        # Display the type and contents of the json_data variable
+        print("json_data is a", type(json_data))
+        pprint(json_data)
+
+#check if this is the main script and execute it
+if __name__ == '__main__':
+        main()
