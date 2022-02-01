09d0302366e67fb75de3e8fc50b2aecd728ff167
Learning-Connections <info@learningconnections.it>
2022-01-31 23:41:33 -0800

    fixed intro_code3.py

--- IntroPython/intro_code3.py
+++ IntroPython/intro_code3.py
@@ -19,32 +19,33 @@ def read_from_file(file):
     """Read from file example"""
     try:
         with open (file, mode='r') as f:
-            text=f.readlines()
+            text=f.read()
             if len(text)>0:
                 return text
-    except: print("File is empty or not exist!")
+            else: 
+                print("File is empty!")
+    except: print("Unable to open the file!")
 
-def modify_text(text):
-    l=input("line to modify? []")
-    text[l]=input("your text: ")
-    return text
+def add_text():
+    t=input("Add text: ")
+    return t+"\n"
 
 def save_to_file(file, text):
     """Save to a text file example"""
     try:
-        with open (file, mode='a') as f:
-            for lines in text:
-                f.writelines()
+        with open (file, 'a') as f:
+            f.write(text)
     except:
         print("Unable to save!")
 
 def menu():
     """Menu Funtion with conditional loop"""
     s=None
+    text=''
     while s!='0':
         print ('**** MENU ****')
         print('1. Read from file')
-        print('2. Modify text')
+        print('2. Add text')
         print('3. Save to file')
         print('0. Exit')
         s=input ('Type your choice: ')
@@ -52,9 +53,12 @@ def menu():
             text=read_from_file(my_file)
             print(text)
         elif s=='2':
-            text=modify_text(text)
+            text=add_text()
+            print (text)
         elif s=='3':
             save_to_file(my_file, text)
+        elif s=='0':
+            quit()
         else:
             print("Select a valid option...")
 
