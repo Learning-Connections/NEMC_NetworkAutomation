c9be3345f1da071eb6d419b26755c500544ea7bf
Learning-Connections <info@learningconnections.it>
2022-02-01 00:43:35 -0800

    added parsingJSON.py and interface-config.json

---
+++ IntroPython/parsingJSON.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# """Module docstring."""
+
+"""parsingJSON.py
+imports, JSON, data structure, files
+"""
+#Imports
+import json
+
+#Constants
+"""no constants"""
+
+#Global Variables
+"""Global Variables Definitions"""
+my_file="interface-config.json"
+
+
+#Functions
